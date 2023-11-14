a3acf559b1cb1de26f81887ae12dbdb81a1dfc59
Priyanshu <priyanshukanji0@gmail.com>
2023-11-14 11:30:43 +0530

    Codes updated

--- Gscript_Channel_Analytics.js
+++ Gscript_Channel_Analytics.js
@@ -1,44 +1,66 @@
-// The onOpen function creates a custom menu in the Google Sheets UI named "Youtube Data."
-// It adds a menu item labeled "Get Channel Data" that calls the getChannelData function.
+// Get the active spreadsheet
+var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+
+// Triggered when the Google Sheets document is opened
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
-  ui.createMenu('Youtube Data')
-    .addItem("Get Channel Data", 'getChannelData')
+  
+  // Create a custom menu named "Youtube Data"
+  var YouTubeData = ui.createMenu('Youtube Data')
+  
+  // Add menu items and associate them with corresponding functions
+  YouTubeData.addItem("Get Channel Data", 'getChannelData')
     .addToUi();
+  
+  // Add another menu item for fetching channels
+  YouTubeData.addItem("Get Channels", 'GetChannels').addToUi();
 }
 
-// The getChannelData function prompts the user to input a YouTube channel ID.
-// It then calls the channelListbyChannelId function with the specified data part and the user-provided channel ID.
+// Function to get YouTube channel data
 function getChannelData() {
   var ui = SpreadsheetApp.getUi();
+  
+  // Prompt the user to input a YouTube channel ID
   var channelId = ui.prompt("Add the channel ID to fetch its analytics").getResponseText();
+  
+  // Call the function to retrieve and append channel data
   return channelListbyChannelId('snippet,contentDetails,statistics', { 'id': channelId });
 }
 
-// The channelListbyChannelId function retrieves data from the YouTube Data API
-// and stores it in the dataRow array. It appends the data to the active spreadsheet.
+// Function to retrieve channel data by channel ID
 function channelListbyChannelId(part, params) {
+  // Make a request to the YouTube Data API
   var response = YouTube.Channels.list(part, params);
 
   if (response.items && response.items.length > 0) {
+    // Extract relevant channel information from the API response
     var channel = response.items[0];
+    
+    // Calculate average views per video
+    var totalView = channel.statistics.viewCount;
+    var totalVideos = channel.statistics.videoCount;
+    var avgViews = totalView / totalVideos;
+    
+    // Construct an array with channel data
     var dataRow = [
       channel.id,
       channel.snippet.title,
-      channel.snippet.title,  // This looks like a duplicate, consider correcting it
-      channel.snippet.email,  // You're trying to access email, but it's not part of the 'snippet'
+      channel.snippet.title,  // (Note: Duplicate entry, consider correcting it)
+      channel.snippet.email,  // (Note: Check if email is available before accessing)
       channel.statistics.subscriberCount,
       channel.statistics.viewCount,
       channel.statistics.videoCount,
-      channel.contentOwnerDetails.contentOwner,
+      avgViews
     ];
-
-    // The channelListbyChannelId function retrieves data from the YouTube Data API
-    // and stores it in the dataRow array. It appends the data to the active spreadsheet.
-    SpreadsheetApp.getActiveSheet().appendRow(dataRow);
-
-    // This method appends the stored data from the dataRow array to a new row in the spreadsheet.
+    
+    // Get the first sheet in the spreadsheet and append the data
+    var firstSheet = spreadsheet.getSheets()[0];
+    firstSheet.appendRow(dataRow);
+    
+    // Return the spreadsheet
+    return spreadsheet;
   } else {
+    // Log an error message if the channel is not found
     Logger.log("Channel not found for the provided ID");
   }
 }
